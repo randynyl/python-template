8e519196061615aec23b58a1b946062e790ff3e9
randynyl <56857427+randynyl@users.noreply.github.com>
2020-09-26 12:08:56 +0800

    Refine salad spree

--- codeitsuisse/routes/salad_spree.py
+++ codeitsuisse/routes/salad_spree.py
@@ -14,20 +14,18 @@ def evaluateSaladSpree():
     print(data)
     logging.info("data sent for evaluation {}".format(data))
     # inputValue = data.get("input");
-    result = []
-
-    for test_case in data:
-        result.append(findTotalPrice(test_case["number_of_salads"], test_case["salad_prices_street_map"]))
+    result = {}
+    result["result"] = findTotalPrice(data["number_of_salads"], data["salad_prices_street_map"])
 
     logging.info("My result :{}".format(result))
-    return json.dumps(result);
+    return jsonify(result);
 
 
 def findTotalPrice(minConsec, mapArr):
     lowest_sum = 100001
     for street in mapArr:
         available_consec_stores = 0
-        for store_index in range(street.len):
+        for store_index in range(len(street)):
             if street[store_index] == "X":
                 available_consec_stores = 0;
                 continue
@@ -36,9 +34,9 @@ def findTotalPrice(minConsec, mapArr):
                 if available_consec_stores >= minConsec:
                     street_price_sum = 0
                     for i in range(minConsec):
-                        street_price_sum += street[store_index - i]
-                        if street_price_sum < lowest_sum:
-                            lowest_sum = street_price_sum
+                        street_price_sum += int(street[store_index - i])
+                    if street_price_sum < lowest_sum:
+                        lowest_sum = street_price_sum
 
     if lowest_sum == 100001:
         return 0
