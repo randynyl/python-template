9a8519be61856cbb6aaedf6cd6f74289f376118e
randynyl <56857427+randynyl@users.noreply.github.com>
2020-09-26 13:23:07 +0800

    refine fruit basket

--- codeitsuisse/routes/fruit_basket.py
+++ codeitsuisse/routes/fruit_basket.py
@@ -24,8 +24,8 @@ def evaluateBasket():
 
 
 def findTotalWeight(qty1, qty2, qty3):
-    weight1 = 82
-    weight2 = 50
+    weight1 = 80
+    weight2 = 48
     weight3 = 50
     total_weight = (qty1 * weight1) + (qty2 * weight2) + (qty3 * weight3)
     return total_weight
