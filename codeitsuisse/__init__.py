b5c1c84ae68ba469c883cb2ad993fee96b5c9a53
randynyl <56857427+randynyl@users.noreply.github.com>
2020-09-26 19:28:34 +0800

    Add clean floor solution

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -4,6 +4,7 @@ import codeitsuisse.routes.salad_spree
 import codeitsuisse.routes.fruit_basket
 import codeitsuisse.routes.gmo_food
 import codeitsuisse.routes.contact_trace
+import codeitsuisse.routes.clean_floor
 
 
 
---
+++ codeitsuisse/routes/clean_floor.py
@@ -0,0 +1,61 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+
+@app.route('/clean_floor', methods=['POST'])
+def evaluateCleanFloor():
+
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    # inputValue = data.get("input");
+    result = {}
+    result["answers"] = {}
+    for key, value in data["tests"].items():
+        result["answers"][key] = cleanFloor(value["floor"])
+
+    logging.info("My result :{}".format(result))
+    return jsonify(result);
+
+
+def cleanFloor(floorArr):
+    current_floor = 0
+    number_of_moves = 0
+    zeroList = [0] * len(floorArr)
+    while floorArr != zeroList:
+        current_floor += chooseDirectionToGo(floorArr, current_floor)
+        if floorArr[current_floor] == 0:
+            floorArr[current_floor] += 1
+        else:
+            floorArr[current_floor] -= 1
+        number_of_moves += 1
+
+    return number_of_moves
+
+
+def chooseDirectionToGo(floorArr, currentFloor):
+    if currentFloor == 0:
+        return 1
+    elif currentFloor == len(floorArr)-1:
+        return -1
+    elif floorArr[currentFloor + 1] > floorArr[currentFloor - 1]:
+        return 1
+    elif floorArr[currentFloor + 1] < floorArr[currentFloor - 1]:
+        return -1
+    else:
+        max_dirt = 0
+        floor_num = -1
+        for i in range(len(floorArr)):
+            if floorArr[i] > max_dirt and i != currentFloor:
+                max_dirt = floorArr[i]
+                floor_num = i
+        if floor_num < currentFloor:
+            return -1
+        elif floor_num > currentFloor:
+            return 1
+
